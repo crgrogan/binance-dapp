40b0a2991daf299347325d21730e38d700160587
Conor Grogan <crgrogan@hotmail.com>
2021-07-05 21:22:07 +0100

    Implement functionality for connecting user's wallet

--- src/App.js
+++ src/App.js
@@ -1,7 +1,41 @@
+import React, { useState } from "react";
 import "./App.css";
+import useWeb3 from "./helpers/useWeb3";
 
 function App() {
-  return <div className="App"></div>;
+  const [address, setAddress] = useState(null);
+  const [balance, setBalance] = useState(null);
+  const web3 = useWeb3();
+
+  const getUserAccount = async () => {
+    if (window.ethereum) {
+      try {
+        await window.ethereum.enable();
+        web3.eth.getAccounts().then((accounts) => {
+          setAddress(accounts[0]);
+          updateBalance(accounts[0]);
+        });
+      } catch (error) {
+        console.error(error);
+      }
+    } else {
+      alert("Metamask extension not detected!");
+    }
+  };
+
+  const updateBalance = async (address) => {
+    await web3.eth.getBalance(address).then((value) => {
+      setBalance(web3.utils.fromWei(value, "ether"));
+    });
+  };
+
+  return (
+    <div className="App">
+      <button onClick={getUserAccount}>Connect</button>
+      <p>Address: {address}</p>
+      <p>Balance: {balance}</p>
+    </div>
+  );
 }
 
 export default App;
